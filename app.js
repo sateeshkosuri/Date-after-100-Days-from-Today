eb3590d8f3c969d245d3cd7d47df495d5b5637d4
Sateesh <sateeshkosuri1997@gmail.com>
2022-08-10 05:46:07 +0000

    Tests for Date After Hundred Days from Today

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const addDays = require("date-fns/addDays");
+const app = express();
+
+app.get("/", (request, response) => {
+  const newDate = addDays(new Date(), 100);
+  response.send(
+    `${newDate.getDate()} /
+      ${newDate.getMonth()} /
+      ${newDate.getFullYear()}`
+  );
+});
+
+app.listen(3000);
+
+module.exports = app;
